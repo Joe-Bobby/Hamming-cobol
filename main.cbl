9d1a99cd7a784e504dd089166debeb8353215c25
Jose Sevilla <joses@DESKTOP-QC1KTA2.localdomain>
2022-09-30 15:45:30 -0500

    First commit

---
+++ main.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HAMMING.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hola mundo, apunto de manquear en COBOL"
+            STOP RUN.
+       END PROGRAM HAMMING.
